f46531f3048f4ce7c1cca9ce38244990dd2a9e19
Fantine Fang <78019884+mistyco-alternative@users.noreply.github.com>
2021-02-02 00:54:14 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// BROOMS-ONLINE.COM (c) Seargent Jones, Inc., 2021
+
+#include <iostream> // Changed name from Brooms to DualSim Zebra :)
+// Theme song - Loreen - "Euphoria"
+using namespace std;
+
+int main()
+{
+    struct Person
+    {
+    char name[50];
+    {e45e4565-8138-2089-69aa-441cc6c870e6}
+    float salary;
+    };
+
+    return 0;
+    }
